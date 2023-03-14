837a26a970b9a281fea52e2776c8c8fcac7a33eb
Jason Kulatunga <jason@thesparktree.com>
2023-03-14 08:57:39 -0700

    fixing changelog function.

--- utils/git/changelog.go
+++ utils/git/changelog.go
@@ -1,51 +1,61 @@
 package git
 
 import (
+	"fmt"
+	goUtils "github.com/analogj/go-util/utils"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/storer"
 	"strings"
 )
 
-//TOOD: enable this.
-// https://github.com/go-git/go-git/issues/36
-//func GitGenerateChangelog(repoPath string, baseSha string, headSha string) (string, error) {
-//	repo, oerr := git2go.OpenRepository(repoPath)
-//	if oerr != nil {
-//		return "", oerr
-//	}
-//
-//	markdown := goUtils.StripIndent(`Timestamp |  SHA | Message | Author
-//	------------- | ------------- | ------------- | -------------
-//	`)
-//
-//	revWalk, werr := repo.Walk()
-//	if werr != nil {
-//		return "", werr
-//	}
-//
-//	rerr := revWalk.PushRange(fmt.Sprintf("%s..%s", baseSha, headSha))
-//	if rerr != nil {
-//		return "", rerr
-//	}
-//
-//	revWalk.Iterate(func(commit *git2go.Commit) bool {
-//		markdown += fmt.Sprintf("%s | %.8s | %s | %s\n", //TODO: this should have a link for the SHA.
-//			commit.Author().When.UTC().Format("2006-01-02T15:04Z"),
-//			commit.Id().String(),
-//			cleanCommitMessage(commit.Message()),
-//			commit.Author().Name,
-//		)
-//		return true
-//	})
-//	//for {
-//	//	err := revWalk.Next()
-//	//	if err != nil {
-//	//		break
-//	//	}
-//	//
-//	//	log.Info(gi.String())
-//	//}
-//
-//	return markdown, nil
-//}
+//https://github.com/go-git/go-git/issues/36
+func GitGenerateChangelog(repoPath string, baseSha string, headSha string) (string, error) {
+	repo, oerr := git.PlainOpen(repoPath)
+	if oerr != nil {
+		return "", oerr
+	}
+
+	markdown := goUtils.StripIndent(`Timestamp |  SHA | Message | Author
+	------------- | ------------- | ------------- | -------------
+	`)
+
+	start, err := repo.ResolveRevision(plumbing.Revision(baseSha))
+	if err != nil {
+		return "", err
+	}
+
+	end, err := repo.ResolveRevision(plumbing.Revision(headSha))
+	if err != nil {
+		return "", err
+	}
+
+	logIter, err := repo.Log(&git.LogOptions{
+		From:  *end,
+		Order: git.LogOrderCommitterTime,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	logIter.ForEach(func(c *object.Commit) error {
+		if c.Hash == *start {
+			return storer.ErrStop
+		}
+
+		markdown += fmt.Sprintf("%s | %.8s | %s | %s\n", //TODO: this should have a link for the SHA.
+			c.Author.When.UTC().Format("2006-01-02T15:04Z"),
+			c.Hash.String(),
+			cleanCommitMessage(c.Message),
+			c.Author.Name,
+		)
+
+		return nil
+	})
+
+	return markdown, nil
+}
 
 // helpers
 func cleanCommitMessage(commitMessage string) string {
