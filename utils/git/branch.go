9413b5e76464acf84772f2f55f588db1b44fbd76
Jason Kulatunga <jason@thesparktree.com>
2023-03-14 21:47:02 -0700

    make sure we create the branch reference

--- utils/git/branch.go
+++ utils/git/branch.go
@@ -26,12 +26,31 @@ func GitCreateBranchFromHead(repoPath string, localBranchName string) (string, e
 		return "", oerr
 	}
 
-	err := repo.CreateBranch(&config.Branch{
-		Name:  localBranchName,
-		Merge: plumbing.NewBranchReferenceName(localBranchName),
-	})
+	//https://github.com/go-git/go-git/blob/master/_examples/branch/main.go
+	headRef, herr := repo.Head()
+	if oerr != nil {
+		return "", herr
+	}
+
+	// Create a new plumbing.HashReference object with the name of the branch
+	// and the hash from the HEAD. The reference name should be a full reference
+	// name and not an abbreviated one, as is used on the git cli.
+	branchRef := plumbing.NewHashReference(plumbing.NewBranchReferenceName(localBranchName), headRef.Hash())
+
+	// The created reference is saved in the storage.
+	err := repo.Storer.SetReference(branchRef)
 	if err != nil {
 		return "", err
 	}
+
+	//this does not actually create a branch reference, just the repo config
+	berr := repo.CreateBranch(&config.Branch{
+		Name:  localBranchName,
+		Merge: plumbing.NewBranchReferenceName(localBranchName),
+	})
+	if berr != nil {
+		return "", berr
+	}
+
 	return localBranchName, nil
 }
